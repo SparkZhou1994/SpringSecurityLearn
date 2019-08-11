b64389bf5e5ce2d523e793f6af5ddf72e44a640e
SparkZhou <zhoujuhui@qq.com>
2019-08-11 22:13:31 +0800

    AuthorizationServerConfigurerAdapter,Lesson 6.10

---
+++ sso-server/src/main/java/spark/sso/SsoServerApplication.java
@@ -0,0 +1,19 @@
+package spark.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName SsoServerApplication
+ * @Description TODO
+ * @Author Spark
+ * @Date 8/11/2019 9:31 PM
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class SsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+}
